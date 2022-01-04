58f9588f07e412368929b72b1feacbbf5224e2aa
Daria <79093855+boikodaria22@users.noreply.github.com>
2022-01-05 01:17:29 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+!/usr/bin/env sh
+
+set -e
+
+
+yarn build
+
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:boikodaria22/HW-Vue-2.git main:gh-pages
+
+cd -
